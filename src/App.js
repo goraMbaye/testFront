8a6115908c2738f22a232c5a631c560e6c63f73b
goraMbaye <g.mbaye3182@zig.univ.sn>
2020-06-23 11:55:19 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Form, Input, InputNumber, Button } from 'antd';
+import { Layout } from 'antd';
+import { Typography } from 'antd';
+
+const { Header, Footer, Sider, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
+  const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+  };
+
+  const validateMessages = {
+    required: '${label} is required!',
+    types: {
+      email: '${label} is not validate email!',
+      number: '${label} is not a validate number!',
+    },
+    number: {
+      range: '${label} must be between ${min} and ${max}',
+    },
+  };
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{background:'white'}}>
+        <Title level={2}>EDITEUR DE FICHIER</Title>
+        </Header>
+        <Content style={{background:'white'}}>
+          <Form style={{margin:0}} {...layout} className="nest-messages" validateMessages={validateMessages}>
+            <Form.Item  label="filetoedit">
+              <Input />
+            </Form.Item>
+            <Form.Item  label="contente file">
+              <Input.TextArea />
+            </Form.Item>
+            <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+              <Button type="primary" htmlType="submit">
+                Save
+            </Button>
+            </Form.Item>
+          </Form>
+
+        </Content>
+      </Layout>
     </div>
   );
+
 }
 
 export default App;
